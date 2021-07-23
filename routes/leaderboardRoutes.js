5efdbbc3e30590b164faf61cd3ae84ebd0b86244
Parker Jones <parker.matthewjones@gmail.com>
2021-07-22 21:50:03 -0500

    basic leaderboard route setup

---
+++ routes/leaderboardRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+
+
+router.get('/', (req, res) => {
+    res.render('leaderboard');
+})
+
+module.exports = router;
