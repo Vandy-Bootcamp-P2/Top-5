9f6bf0ff7050ee5739be7f1e972ae4270d77209b
Parker Jones <parker.matthewjones@gmail.com>
2021-07-22 22:14:02 -0500

    initial title query

--- routes/leaderboardRoutes.js
+++ routes/leaderboardRoutes.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = require('express').Router();
 const sequelize = require('../config/connection');
+const middleware = require('../middleware');
+const Post = require('../models/Post.js');
+const { Op } = require("sequelize");
 
 
-router.get('/', (req, res) => {
+router.get('/', middleware.requireLogin, (req, res) => {
+    Post.findAll({
+        attributes: [
+            [sequelize.fn('DISTINCT', sequelize.col('title')) , 'posts'],
+        ],
+        where: {
+            title: {
+                [Op.not]: '',
+            }}
+    }).then(function(posts) { 
+        console.log(posts);
+     })
+
     res.render('leaderboard');
 })
 
