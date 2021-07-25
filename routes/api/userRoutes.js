d76858f5a2ad0a2eae2e457f2906d4e1fed11d01
michaelab24 <michaelbarnes459@yahoo.com>
2021-07-24 20:03:41 -0500

    to make myself look better

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -34,4 +34,15 @@ router.get('/:id', (req, res) => {
     });
 });
 
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
+});
+
 module.exports = router;
--- routes/logoutRoute.js
+++ routes/logoutRoute.js
@@ -8,17 +8,34 @@ router.get('/', (req, res) => {
     res.status(200).render('login');
 })
 
-router.get('/logout', function(req, res) {
-    req.logout();
-    if (!req.session) {
-        req.session.destroy(function(err) {
-            res.redirect('/login');
-        });
+
+async function logout() {
+    const response = await fetch('/logout', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/login');
+    } else {
+      alert(response.statusText);
     }
-    else {
-        res.redirect('/login');
-    }
-});
+  }
+
+  document.querySelector('#logout').addEventListener('click', logout);
+
+
+// router.get('/logout', function(req, res) {
+//     req.logout();
+//     if (!req.session) {
+//         req.session.destroy(function(err) {
+//             res.redirect('/login');
+//         });
+//     }
+//     else {
+//         res.redirect('/login');
+//     }
+// });
 
 
 
