7e031b01c1f4ed756efdaca9c20245c897484e81
Jeremy's commits <jeremyhurst16@gmail.com>
2021-07-25 20:59:03 -0400

    fixed merge errors

--- public/javascript/common.js
+++ public/javascript/common.js
@@ -1,12 +1,10 @@
 const { response } = require("express");
 const { increment } = require("../../models/User");
 const { post } = require("../../routes/api/postRoutes");
-
 // enable submit button when use enters text
 $(".list-group").keyup(() => {
     var postFieldAny = $(event.target);
     var value = postFieldAny.val().trim();
-
     var submitButton = $("#submitPostButton");
     if (value == "") {
         submitButton.prop("disabled", true);
@@ -14,11 +12,9 @@ $(".list-group").keyup(() => {
     }
     submitButton.prop('disabled', false);
 });
-
 // send text in textfields to postRoutes when user clicks submit
 $("#submitPostButton").click((event) => {
     var button = $(event.target);
-
     var postFieldAny = getPostFieldAny();
     var postCategory = $("#postCategory")
     var postCaption = $("#postCaption");
@@ -27,7 +23,6 @@ $("#submitPostButton").click((event) => {
     var postField3 = $("#postField3")
     var postField4 = $("#postField4")
     var postField5 = $("#postField5")
-
     var data = {
         title: postCategory.val(),
         caption: postCaption.val(),
@@ -52,59 +47,49 @@ $("#submitPostButton").click((event) => {
         button.prop("disabled", true);
     })
 })
-
 //function for clicking the like button, and getting the dynamic response
-// document.addEventListener("DOMContentLoaded", function() {
-//     fetch(`http://localhost3001`)
-//     .then(response => response.json())
-//     .then((postData) => {
-//         addLikes(postData)
-//     })
-// })
-
-// function addLikes(postData){
-//     const likesCounter = document.querySelector('.likes')
-//     likesCounter.innerText - `${postData.likes} likes`
-
-//     const likeButton = document.querySelector('.likeButton')
-
-//     likeButton.innerText = incrementLikes(postData)
-// }
-
-// function incrementLikes(postData) {
-//     //new variable for likes set to 0
-//     let likes = 0
-//     //insert a new end point if we end up taking the post to a new page!!--
-//     //also might not be post.id, just plugging in for now till you guys have front end done, some changes may need to happen to line up everything!
-//     fetch(`http://localhost3001/${postData.id}`)
-//     .then(response => response.json())
-//     .then((postData) => {
-//         likes = postData.likes
-//     })
-// //incrementing the likes for every time someone likes the post 
-//     let newLikes = likes + 1
-
-//     fetch(`http://localhost3001/`, {
-//         method: 'PATCH',
-//         headers: {
-//             "Content-Type": "application/json",
-//             Accept: "application/json"
-//         },
-//         body: JSON.stringify({
-//             "likes": newLikes
-//         })
-//     })
-//     let likesText = `${newLikes} likes`
-//     return likesText
-// }
-
-
+document.addEventListener("DOMContentLoaded", function () {
+    fetch(`http://localhost3001`)
+        .then(response => response.json())
+        .then((postData) => {
+            addLikes(postData)
+        })
+})
+function addLikes(postData) {
+    const likesCounter = document.querySelector('.likes')
+    likesCounter.innerText - `${postData.likes} likes`
+    const likeButton = document.querySelector('.likeButton')
+    likeButton.innerText = incrementLikes(postData)
+}
+function incrementLikes(postData) {
+    //new variable for likes set to 0
+    let likes = 0
+    //insert a new end point if we end up taking the post to a new page!!--
+    //also might not be post.id, just plugging in for now till you guys have front end done, some changes may need to happen to line up everything!
+    fetch(`http://localhost3001/${postData.id}`)
+        .then(response => response.json())
+        .then((postData) => {
+            likes = postData.likes
+        })
+    //incrementing the likes for every time someone likes the post 
+    let newLikes = likes + 1
+    fetch(`http://localhost3001/`, {
+        method: 'PATCH',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            "likes": newLikes
+        })
+    })
+    let likesText = `${newLikes} likes`
+    return likesText
+}
 // $(document).on("click", ".likeButton", (event) => {
 //     var button = $(event.target);
 //     var postId = getPostIdFromElement(button);
-
 //     if(postId === undefined) return;
-
 //     $.ajax({
 //         url: `/api/postRoutes/${postId}/likes`,
 //         type: "PUT",
@@ -113,7 +98,6 @@ $("#submitPostButton").click((event) => {
 //         }
 //     })
 // })
-
 //post id function to get to the root element
 // function getPostIdFromElement(element) {
 //     var isRoot = element.hasClass("post")
@@ -122,17 +106,10 @@ $("#submitPostButton").click((event) => {
 //     var rootElement = isRoot ? element : element.closest(".post");
 //     //connects this to the card with the data-id
 //     var postId = rootElement.data().id
-
 //     if(postId === undefined)
 //     return alert(" Post id undefined ");
-
 //     return postId;
 // }
-
-//comments section
-
-
-
 //function to detect if there in input within the post
 function getPostFieldAny(event) {
     $(".list-group").keyup(() => {
@@ -141,31 +118,8 @@ function getPostFieldAny(event) {
         return value;
     })
 };
-
 //will print the post to the screen
 function createPostHtml(postData) {
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-    return `<div class="card" style="width: 18rem;">
-    <img src="" class="card-img-top" alt="...">
-    <div class="card-body" data-id="">
-      <h5 class="card-title"></h5>
-        <p class="card-text"></p>
-        <p class="card-text"></p>
-        <p class="card-text"></p>
-        <p class="card-text"></p>
-        <p class="card-text"></p>
-        <div class="likes-section">
-            <span class="likes>0 likes</span>
-            <button class="likeButton">Like +</button
-        </div>
-    </div>
-  </div>`
-=======
-
->>>>>>> 26d49d298b46da87b892f361d700dde0f9a00d04
     return `
     <div class="postFormContainer">
     <div class="textareaContainer">
@@ -193,29 +147,17 @@ function createPostHtml(postData) {
         </div>
     </div>
 </div>
-
 `
 }
-
 function createLeaderboard(postData) {
-
     return ` 
                 <a href="http://localhost:3001/leaderboard/${postData.title}">${postData.title}</a>
                 <br>
-            
     `
 }
-
 function createLeaderboardCategory(postData) {
-
     return ` 
                 <p>${postData.title}</p>
                 <br>
-            
-<<<<<<< HEAD
     `
-=======
-    `
->>>>>>> f36e66650cb69acf8f6efd27160b1f08e6d08ffd
->>>>>>> 26d49d298b46da87b892f361d700dde0f9a00d04
 }
