d3b7328acb7405b1606b420567f9868b38472f8f
Jeremy's commits <jeremyhurst16@gmail.com>
2021-07-27 00:37:37 -0400

    fixed url for leaderboards before pushing to main

--- public/javascript/common.js
+++ public/javascript/common.js
@@ -150,7 +150,7 @@ function createPostHtml(postData) {
 function createLeaderboard(postData) {
     return ` 
         <div class="d-grid gap-2 col-6 mx-auto d-flex flex-column justify-content-center">
-            <a href="http://localhost:3001/leaderboard/${postData.title}" type="button" class="btn btn-outline-primary">${postData.title}</a>
+            <a href="https://top-5-list.herokuapp.com/leaderboard/${postData.title}" type="button" class="btn btn-outline-primary">${postData.title}</a>
         </div>
     `
 }
